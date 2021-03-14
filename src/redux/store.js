2fd0d9b6f2f2c5e28e62b00d8ee077c115a6003e
Rabii-Ch <rabii_chandra@hotmail.fr>
2021-03-14 19:20:51 +0100

    add shop reducer and filter collection by categorie..

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,3 @@
-import{ createStore, applyMiddleware} from 'redux';
-import logger from 'redux-logger';
-
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
@@ -12,3 +9,5 @@ const middlewares = [logger];
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
+
+export default { store, persistStore };
